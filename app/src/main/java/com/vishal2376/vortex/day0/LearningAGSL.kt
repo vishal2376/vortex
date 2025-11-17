f388da009c7d2ae8a971a4804abe982860cafc64
Vishal Singh <vishalsingh2376@gmail.com>
2025-11-18 00:02:05 +0530

    feat: day0 - customize shaders with parameters

--- app/src/main/java/com/vishal2376/vortex/day0/LearningAGSL.kt
+++ app/src/main/java/com/vishal2376/vortex/day0/LearningAGSL.kt
@@ -5,14 +5,22 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ShaderBrush
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.vishal2376.vortex.ui.theme.VortexTheme
 import org.intellij.lang.annotations.Language
+
+object Constants {
+	const val RESOLUTION = "resolution"
+	const val COLOR_A = "colorA"
+	const val COLOR_B = "colorB"
+}
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
@@ -20,14 +28,25 @@ fun LearningAGSL() {
 
 	@Language("AGSL")
 	val shaderInfo = """
+		uniform float2 ${Constants.RESOLUTION};
+		layout(color) uniform half4 ${Constants.COLOR_A};
+		layout(color) uniform half4 ${Constants.COLOR_B};
+		
 		half4 main(in float2 fragCoord) {
-			return half4(1,1,0,1);
+			float2 uv = fragCoord / ${Constants.RESOLUTION}.xy;
+			float mixValue = distance(uv, vec2(0,1));
+			
+			return mix(${Constants.COLOR_A}, ${Constants.COLOR_B} , mixValue);
 		}
 	""".trimIndent()
 
 	Box(
 		modifier = Modifier
-			.applyShader(shaderInfo)
+			.applyShader(
+				shaderInfo = shaderInfo,
+				colorA = MaterialTheme.colorScheme.background,
+				colorB = MaterialTheme.colorScheme.primary
+			)
 			.size(200.dp)
 	)
 }
@@ -35,19 +54,42 @@ fun LearningAGSL() {
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 private fun Modifier.applyShader(
 	shaderInfo: String,
+	colorA: Color,
+	colorB: Color,
 ): Modifier {
-
-	val shader = RuntimeShader(shaderInfo)
-	val shaderBrush = ShaderBrush(shader)
-
 	return drawWithCache {
+
+		val shader = RuntimeShader(shaderInfo)
+		val shaderBrush = ShaderBrush(shader)
+
+		//set variable
+		shader.setFloatUniform(Constants.RESOLUTION, size.width, size.height)
 		onDrawBehind {
+			shader.setColorUniform(
+				Constants.COLOR_A,
+				android.graphics.Color.valueOf(
+					colorA.red,
+					colorA.green,
+					colorA.blue,
+					colorA.alpha,
+				)
+			)
+			shader.setColorUniform(
+				Constants.COLOR_B,
+				android.graphics.Color.valueOf(
+					colorB.red,
+					colorB.green,
+					colorB.blue,
+					colorB.alpha,
+				)
+			)
 			drawRect(shaderBrush)
 		}
 	}
 }
 
-@Preview
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@PreviewLightDark
 @Composable
 private fun LearningAGSLPreview() {
 	VortexTheme() {
